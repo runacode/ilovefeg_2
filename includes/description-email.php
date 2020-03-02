8e89e5a8459aecf2c86ec418c2bf2c8a4c9fb82d
Ethan <ethan@ewuddy.com>
2020-03-02 12:22:20 -0500

    retarget

---
+++ includes/description-email.php
@@ -0,0 +1,93 @@
+<h2 class="featured-in text-uppercase text-center mt-3"><span><?= T('Featured In'); ?></span></h2>
+<div class="logos mt-5">
+Thanks for opening our emails</div>
+<div class="three-benefites three-benefited__count-four d-flex justify-content-between mt-5">
+    
+</div>
+
+<div class="magic-grid mt-5">
+
+
+</div>
+
+
+<div class="magic-grid mt-5">
+
+    <div class="d-flex flex-row">
+        <div>
+
+            <img class="img-fluid" src="../resources/images/100-natural-img.jpg">
+        </div>
+    </div>
+
+    <div class="d-flex flex-row">
+        <div>
+    </div>
+
+    <div class="d-flex flex-row">
+        
+    </div>
+
+    <div class="d-flex flex-row">
+       
+    <div class="d-flex flex-row">
+       </div>
+
+<div class="red-arrow-next-line">
+    <img src="../resources/images/red_arrow_next_line.svg" alt>
+</div>
+
+
+
+
+<div class="daily_treatment_section mt-5">
+    <div class="row">
+      
+    </div>
+    <div class="row">
+        
+    </div>
+</div>
+
+
+<div class="guaranteed-satisfaction mt-5">
+    <div class="text-center">
+        <img src="../resources/images/guarantee-seal.png" alt>
+    </div>
+   
+
+
+
+<div class="container">
+    <div class="accordion" id="accordionExample">
+        <div class="card">
+            <div class="card-header" id="headingOne">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                            data-target="#collapseOne" aria-expanded="false"
+                            aria-controls="collapseOne"><?= T('FAQ'); ?></button>
+                </h2>
+            </div>
+
+            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                <div class="card-body">
+                    <?php include 'faq.php' ?>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="headingTwo">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                            data-target="#collapseTwo" aria-expanded="false"
+                            aria-controls="collapseTwo"><?= T('Contact Us'); ?></button>
+                </h2>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                <div class="card-body">
+                    <?php include 'contact-us.php' ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
